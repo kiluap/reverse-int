cf73891d86910cc4308eaf4a7e2d316e02cd1b99
Pavel Skrypko <V_8_P@mail.ru>
2020-10-22 20:57:19 +0300

    fix: fixed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let hun = Math.trunc(num/100);
+  let dec = Math.trunc(num/10) % 10;
+  let ed = num % 10;
+  return hun + dec * 10 + ed * 100;
 }
